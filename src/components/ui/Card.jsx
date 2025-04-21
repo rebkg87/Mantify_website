1402d7ce41b46b39546c55dbac4273c46a912d4d
Rebeca Garcia <rebe.ga87@gmail.com>
2025-04-21 16:55:50 +0200

    fixing card component styles

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 
 const Card = ({ title, description, number }) => {
     return (
-        <div className="group relative max-w-xs w-52 h-56 bg-gradient-to-tl from-primary to-secondary dark:from-tertiary dark:to-white p-10 shadow-xl transform transition-all hover:scale-105 hover:shadow-2xl hover:bg-secondary  dark:bg-white rounded-xl">
-            <div className="absolute top-0 left-0 w-full h-full bg-secondary dark:bg-primary transform scale-y-0 transition-transform duration-500 group-hover:scale-y-100 origin-top group-hover:origin-bottom p-7 rounded-xl">
-                <h2 className="absolute left-10 top-12 text-4xl font-extrabold opacity-50 transition-opacity duration-500 group-hover:opacity-100 transform group-hover:translate-y-[-40px] group-hover:text-tertiary text-secondary dark:text-tertiary">
+        
+        <div className="group relative max-w-xs w-60 h-52 bg-gradient-to-tl from-primary to-secondary dark:from-tertiary dark:to-white p-10 shadow-xl transform transition-all hover:scale-105 hover:shadow-2xl hover:bg-white dark:bg-white rounded-xl">
+                <h2 className="pt-4 absolute left-10 top-2 group-hover:top-8 text-4xl font-extrabold opacity-10 transition-opacity duration-500 group-hover:opacity-100 transform group-hover:translate-y-[-40px] group-hover:text-tertiary dark:text-secondary text-tertiary">
                     {number}
                 </h2>
-                <h3 className='relative text-lg font-semibold mb-4 text-tertiary dark:text-secondary group-hover:text-white dark:group-hover:text-tertiary transition-all duration-500 group-hover:mt-10'>
+                <h3 className='relative text-lg font-semibold mb-4 dark:text-secondary text-tertiary opacity-70 dark:group-hover:text-primary group-hover:text-white transition-all duration-500 group-hover:mt-6'>
                     {title}
                 </h3>
                 <p className="text-tertiary dark:text-secondary">
                     {description}
                 </p>
-            </div>
         </div>
     )
 }
